321517800fe9dad4c785f517bbee7e623b1d1be4
Nelson Elhage <nelhage@nelhage.com>
2014-03-13 11:26:52 -0700

    Add a test for (rollup 1 N) with a quiescent period

--- test/riemann/streams_test.clj
+++ test/riemann/streams_test.clj
@@ -1235,7 +1235,12 @@
            (test-stream-intervals
              (rollup 1 2)
              [:a 0 :b 0 :c 2 :d 0 :e 0 :f 2]
-             [[:a] [:b :c] [:d :e :f]]))
+             [[:a] [:b :c] [:d :e :f]])
+
+           (test-stream-intervals
+             (rollup 1 2)
+             [:a 0 :b 0 :c 10 :d 0 :e 0 :f 2]
+             [[:a] [:b :c] [:d] [:e :f]]))
 
          (testing "basic rollups"
                   (test-stream-intervals 
